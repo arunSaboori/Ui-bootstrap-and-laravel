02a34c740d39649eb527f69c1305389ae1319150
arun <arun.saboori@gmail.com>
2022-08-09 00:18:34 +0430

    second-commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital@1&display=swap" rel="stylesheet">
     <title>bootsrap ui page for training </title>
 </head>
-<body>
 
-<a href="" class="btn btn-primary"> fuck you bnt</a>
+<body>
+    <header class="navbar navar-expand-lg  sticky-top  px-5 navbar1">
+        <div class="container-fluid">
+            <img src="{{ asset('img/joystick.svg') }}" class="img1">
+            <a href="" class="navbar-brand">mycrud</a>
+            <a href="" class="navbar-brand">home</a>
+            <a href="" class="navbar-brand">about me :\</a>
+
+        </div>
+
+
+
+    </header>
+
+
+    <section class="">
+        <h3 class="container-fluid text1  "> ! dear users <br> <span class="opacity-75">i make crud for free Yeap</span>
+        </h3>
+        <div class="div1"> <a href="" class="btn btn2 rounded-pill">get started</a> <a href="" class="btn btn3 rounded-pill">try now</a>
+        </div>
+        <img src="{{ asset('img/pexels-eda-11482477.jpg') }}" class="img-fluid  img2">
+
+
+    </section>
+    <aside class="container-fluid ">
+      <h1 class="aside-text">well we are doing this <br> for years now</h1>
+
+
+
+
+
+
+
+
+
+    </aside>
+    <nav></nav>
+    <footer></footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
