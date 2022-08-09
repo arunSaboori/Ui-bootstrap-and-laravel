e705c553b4bc163dc40be1d0be1547e925e1e0bd
arun <arun.saboori@gmail.com>
2022-08-09 08:01:18 +0430

    almost done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" dir="rtl">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -29,29 +29,70 @@
     </header>
 
 
-    <section class="">
+    <section>
         <h3 class="container-fluid text1  "> ! dear users <br> <span class="opacity-75">i make crud for free Yeap</span>
         </h3>
-        <div class="div1"> <a href="" class="btn btn2 rounded-pill">get started</a> <a href="" class="btn btn3 rounded-pill">try now</a>
+        <div class="div1"> <a href="" class="btn btn2 rounded-pill">get started</a> <a href=""
+                class="btn btn3 rounded-pill">try now</a>
         </div>
         <img src="{{ asset('img/pexels-eda-11482477.jpg') }}" class="img-fluid  img2">
 
 
     </section>
-    <aside class="container-fluid ">
-      <h1 class="aside-text">well we are doing this <br> for years now</h1>
+    <aside class="container-fluid">
+        <h1 class="aside-text">well we are doing this <br> for years now</h1>
 
-
-
-
-
+        <div class="container-fluid divimg">
+            <div>
+                <p>its easy to use</p>
+                <img src="{{ asset('img/emoji-smile.svg') }}">
+            </div>
+            <div>
+                <p>24 hourse cash money back</p>
+                <img src="{{ asset('img/cash.svg') }}">
+            </div>
+            <div>
+                <p>its safe and secure</p>
+                <img src="{{ asset('img/file-lock-fill.svg') }}">
+            </div>
+            <div>
+                <p>its fast</p>
+                <img src="{{ asset('img/fast-forward-btn.svg') }}">
+            </div>
+        </div>
 
 
 
 
     </aside>
-    <nav></nav>
-    <footer></footer>
+
+    <footer class="container-fluid bg-dark text-center">
+     <div class="divfooter"> 
+        <div class="conteainer">
+            <div>
+                <ul class="list-unstyled mb-0 ul1">
+                    <div class="text-light fs-4">about us</div>
+                    <li> <a href="" class=" ">about us</a></li>
+                    <li> <a href="">join now</a></li>
+                    <li> <a href="">about crud</a></li>
+                    <li> <a href="">learn more</a></li>
+                </ul>
+            </div>
+
+           </div >
+           <ul class="list-unstyled mb-0 ">
+            <div class="text-light fs-4">connect us</div>
+            <li> <a href="">contect us</a></li>
+            <li> <a href="">need more workers</a></li>
+            <li> <a href="">carees</a></li>
+            </div>
+         </ul>
+    </div>
+
+        <div>
+            <h5 class="text-light last-text"> all content are served to this website arun.saboori 2012 - 2022</h5>
+        </div>
+    </footer>
 
 
 
